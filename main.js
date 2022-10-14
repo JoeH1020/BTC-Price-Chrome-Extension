8a3b93dca7060433670ea1fe59078f606987ee5a
Joe Hynan <joehynan@joes-air.lan>
2022-10-13 22:12:42 -0500

    completed fetch

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-    const price = 18348;
-    const display = document.getElementById('btcPrice');
-    display.innerHTML = `1 BTC = $${price} USD`
+  let price = 0;
+  const display = document.getElementById('btcPrice');
+
+
+
+
+  fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+      .then((data) => data.json())
+      .then((data) => {
+       price = data.bitcoin.usd;
+       display.innerHTML = `1 BTC = $${price} USD`
+      console.log('test');
+    });
+
+
+
 });
