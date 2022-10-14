4063e4fb5b04150c6ede18640b397387a2121f96
Joe Hynan <joehynan@joes-air.lan>
2022-10-13 20:06:41 -0500

    created files

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const price = 18348;
+    const display = document.getElementById('btcPrice');
+    display.innerHTML = `1 BTC = $${price} USD`
+});
